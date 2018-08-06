06b9152bba476a93ad5bfc1e0eb598d02cd23004
luis <info@innocuo.com>
2018-08-06 00:27:59 -0500

    experimenting with delaying increment

--- p5sketches/displaynoise6/sketch.js
+++ p5sketches/displaynoise6/sketch.js
@@ -9,15 +9,17 @@ var current_pos={
 }
 
 let curr_angle = 0;
-let curr_speed=13;
-let curr_inc = -0.5;
+let curr_speed=20;
+let curr_inc = 28;
+let inc_every = 1;
+let curr_inc_every = 0;
 
 let curr_angle2 = 90;
 let curr_inc2 = 10;
 
 let pix_val = 0;
 
-let row_height = 1;
+let row_height = 8;
 
 function setup() {
 
@@ -55,7 +57,8 @@ function draw() {
 	for (let row = 0; row< row_total; row+=row_height){
 
 		let tmp_angle = curr_angle;
-
+		curr_inc_every=0;
+//tmp_angle=0;
 		//move from left to right
 		for(var col=0; col<16; col++){
 			curr_angle2 += curr_inc2 ;
@@ -70,10 +73,13 @@ function draw() {
 				//console.log(curr_angle, sin1)
 				//console.log(sin1)
 
-				tmp_angle += get_inc()*(row+1);
+				tmp_angle += get_inc()*(row+1)*(row+1);
+				// tmp_angle += get_inc();
 				let sin1 = sin(tmp_angle*PI/180);
 
+
 				let wave_val = sin1+1; //now values go from 0 to 2;
+
 				let pixel_pos = (int(wave_val*((row_total*row_height)-1)/2));
 
 				let cache_pos = {x: current_pos.x, y: current_pos.y };
@@ -82,10 +88,11 @@ function draw() {
 					let pixel_divided = pixel_pos%8 ;
 					//pixel_divided = pixel_divided<<1<<(pixel_divided+1);
 					let pixel2= 0 ;
-					for(var lo=0;lo<2;lo++){
+					for(var lo=0;lo<inc_every/2;lo++){
 						pixel2 |= 1<< (pixel_divided-lo)
-						pixel2 |= 1<< (pixel_divided+lo)
+						//pixel2 |= 1<< (pixel_divided+lo)
 					}
+
 
 					moveTo(cache_pos.x, cache_pos.y + (row_height-ii-1));
 
@@ -110,8 +117,12 @@ function draw() {
 }
 
 function get_inc(){
-
-	return curr_inc;
+	curr_inc_every++;
+	if(curr_inc_every == inc_every){
+		curr_inc_every = 0;
+		return curr_inc;
+	}
+	return 0;
 }
 
 function moveTo(x, y){
