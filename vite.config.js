4fed2565a4c4435843674be3b923be769da75fd3
Luis Orozco <6557454+innocuo@users.noreply.github.com>
2026-03-11 23:09:17 -0500

    added base path

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  base: "/displaynoise/",
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
   },
 });
