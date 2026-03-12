0b6c92c4f1d44e05aa97ba96eaa7227933150f47
Luis Orozco <6557454+innocuo@users.noreply.github.com>
2026-03-11 23:15:38 -0500

    fixing routes path for publishing

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import HomeView from "@/views/HomeView.vue";
 import SketchPlayerView from "@/views/SketchPlayerView.vue";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
